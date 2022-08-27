5b85dd7a61956cf29fbc4a800dcfd805aabc785a
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-08-27 10:35:10 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+      fetchImages()
+      fetchBreeds()
+})
+
+function fetchImages() {
+      const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+      fetch(imgUrl)
+            .then(resp => resp.json())
+            .then(json => renderImages(json));
+}
+
+function renderImages(json) {
+      const main = document.getElementById('dog-image-container')
+      json.message.forEach(image => {
+            const img = document.createElement('img')
+            img.src = image
+            main.appendChild(img)
+      })
+}
+
+
+function fetchBreeds() {
+      const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+      fetch(breedUrl)
+            .then(resp => resp.json())
+            .then(json => renderBreeds(json));
+}
+
+function renderBreeds(json) {
+      const ul = document.getElementById('dog-breeds')
+      for (const breed in json.message) {
+            const li = document.createElement('li')
+            li.innerText = breed
+            ul.appendChild(li)
+            li.addEventListener('click', function () {
+                  li.style.color = 'red'
+            })
+      }
+}
+
+// Language: javascript
+
